b44005eb7ea740b58742b9725fb93622565392bf
lja3723 <lja3723@gmail.com>
2024-09-13 15:32:50 +0900

    [Weekly/2] 우주인 프로젝트 초기화 (#1)

---
+++ src/main/java/org/ktc2/cokaen/wouldyouin/WouldYouInApplication.java
@@ -0,0 +1,13 @@
+package org.ktc2.cokaen.wouldyouin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WouldYouInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WouldYouInApplication.class, args);
+    }
+
+}
