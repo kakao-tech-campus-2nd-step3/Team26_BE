469799f871b6b780e5dd5638dfec3c8a8be4ae50
lja3723 <lja3723@gmail.com>
2024-09-13 15:32:50 +0900

    우주인 프로젝트 초기화

---
+++ src/test/java/org/ktc2/cokaen/wouldyouin/WouldYouInApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ktc2.cokaen.wouldyouin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WouldYouInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
