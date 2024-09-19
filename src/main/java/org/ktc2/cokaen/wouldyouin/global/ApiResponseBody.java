69ccca9ad3be963810a841a405f82d128108d31c
lja3723 <lja3723@gmail.com>
2024-09-19 19:30:08 +0900

    refactor: ApiResponseBody 클래스를 global로 이동

--- src/main/java/org/ktc2/cokaen/wouldyouin/global/ApiResponseBody.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/global/ApiResponseBody.java
@@ -1,12 +1,10 @@
-package org.ktc2.cokaen.wouldyouin.controller.event.response;
+package org.ktc2.cokaen.wouldyouin.global;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-//globalwouldyouin.global.ApiResponseBody로 사용 필요
-@Deprecated
 public class ApiResponseBody<D> {
 
     private final Boolean status;
