08f83bb0190e454c62d186c0e51242816120842f
lja3723 <lja3723@gmail.com>
2024-09-27 22:50:24 +0900

    feat: Host, Curator 컨트롤러 추가 및 Member 컨트롤러 변경

---
+++ src/main/java/org/ktc2/cokaen/wouldyouin/controller/member/CuratorController.java
@@ -0,0 +1,34 @@
+package org.ktc2.cokaen.wouldyouin.controller.member;
+
+import lombok.RequiredArgsConstructor;
+import org.ktc2.cokaen.wouldyouin.controller.member.dto.CuratorEditRequest;
+import org.ktc2.cokaen.wouldyouin.controller.member.dto.MemberResponse;
+import org.ktc2.cokaen.wouldyouin.domain.MemberType;
+import org.ktc2.cokaen.wouldyouin.global.ApiResponseBody;
+import org.ktc2.cokaen.wouldyouin.global.annotation.Authorize;
+import org.ktc2.cokaen.wouldyouin.service.member.CuratorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/curators")
+public class CuratorController {
+
+    private final CuratorService curatorService;
+
+    @PutMapping
+    public ResponseEntity<ApiResponseBody<MemberResponse>> updateCurator(@Authorize(MemberType.curator) Long curatorId, @RequestBody CuratorEditRequest request) {
+        return ResponseEntity.ok(new ApiResponseBody<>(true, curatorService.updateCurator(curatorId, request)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponseBody<MemberResponse>> createCurator(@Authorize(MemberType.normal) Long normalMemberId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponseBody<>(true, curatorService.createCurator(normalMemberId)));
+    }
+}
--- src/main/java/org/ktc2/cokaen/wouldyouin/controller/member/MemberController.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/controller/member/MemberController.java
@@ -3,10 +3,13 @@ package org.ktc2.cokaen.wouldyouin.controller.member;
 import lombok.RequiredArgsConstructor;
 import org.ktc2.cokaen.wouldyouin.controller.member.dto.MemberCreateRequest;
 import org.ktc2.cokaen.wouldyouin.controller.member.dto.MemberEditRequest;
+import org.ktc2.cokaen.wouldyouin.controller.member.dto.MemberResponse;
 import org.ktc2.cokaen.wouldyouin.domain.MemberType;
 import org.ktc2.cokaen.wouldyouin.global.ApiResponseBody;
 import org.ktc2.cokaen.wouldyouin.global.annotation.Authorize;
 import org.ktc2.cokaen.wouldyouin.service.member.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,19 +19,21 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    //TODO: MemberSearchService 이용 필요
     @GetMapping("{memberId}")
-    public ApiResponseBody<?> findMember(@PathVariable("memberId") Long memberId) {
-        return new ApiResponseBody<>(true, memberService.getById(memberId));
+    public ResponseEntity<ApiResponseBody<MemberResponse>> findMember(@PathVariable("memberId") Long memberId) {
+        return ResponseEntity.ok(new ApiResponseBody<>(true, memberService.getById(memberId)));
     }
 
     @PostMapping
-    public ApiResponseBody<?> createMember(@RequestBody MemberCreateRequest createRequest) {
-        return new ApiResponseBody<>(true, memberService.createMember(createRequest));
+    public ResponseEntity<ApiResponseBody<MemberResponse>> createMember(@RequestBody MemberCreateRequest createRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(new ApiResponseBody<>(true, memberService.createMember(createRequest)));
     }
 
     @PutMapping
-    public ApiResponseBody<?> modifyMember(@Authorize(MemberType.normal) Long memberId, @RequestBody MemberEditRequest editRequest) {
-        return new ApiResponseBody<>(true, memberService.updateMember(memberId, editRequest));
+    public ResponseEntity<ApiResponseBody<MemberResponse>> updateMember(@Authorize(MemberType.normal) Long memberId, @RequestBody MemberEditRequest editRequest) {
+        return ResponseEntity.ok(new ApiResponseBody<>(true, memberService.updateMember(memberId, editRequest)));
     }
 
     @DeleteMapping
