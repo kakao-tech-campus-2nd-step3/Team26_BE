0aba8583dbcf0c2eed6899c4b1151c90e3146ad0
daolove0323 <daolove0323@gmail.com>
2024-10-02 15:36:20 +0900

    feat: EventService unit test

--- src/test/java/org/ktc2/cokaen/wouldyouin/global/TestData.java
+++ src/test/java/org/ktc2/cokaen/wouldyouin/global/TestData.java
@@ -1,19 +1,50 @@
 package org.ktc2.cokaen.wouldyouin.global;
 
 import java.time.LocalDateTime;
-import org.ktc2.cokaen.wouldyouin.event.api.EventRequest;
-import org.ktc2.cokaen.wouldyouin.reservation.application.dto.ReservationRequest;
 import org.ktc2.cokaen.wouldyouin._common.persist.Area;
 import org.ktc2.cokaen.wouldyouin._common.persist.Category;
 import org.ktc2.cokaen.wouldyouin._common.persist.Location;
+import org.ktc2.cokaen.wouldyouin.event.api.EventRequest;
+import org.ktc2.cokaen.wouldyouin.event.persist.Event;
+import org.ktc2.cokaen.wouldyouin.reservation.application.dto.ReservationRequest;
 
 public class TestData {
 
     public static EventRequest validEventRequest;
+    public static EventRequest validEventRequestToModify;
+    public static Event validEvent;
     public static ReservationRequest validReservationRequest;
 
     static {
         validEventRequest = EventRequest.builder()
+            .hostId(1L)
+            .title("title")
+            .content("content")
+            .area(Area.전체)
+            .location(new Location(132.0, 43.0))
+            .startTime(LocalDateTime.of(2024, 10, 1, 9, 0))
+            .endTime(LocalDateTime.of(2024, 10, 1, 10, 0))
+            .price(10000)
+            .totalSeat(100)
+            .leftSeat(50)
+            .category(Category.밴드)
+            .build();
+
+        validEventRequestToModify = EventRequest.builder()
+            .hostId(2L)
+            .title("modifiedTitle")
+            .content("modifiedContent")
+            .area(Area.광주)
+            .location(new Location(232.0, 143.0))
+            .startTime(LocalDateTime.of(2024, 10, 2, 17, 0))
+            .endTime(LocalDateTime.of(2024, 10, 2, 18, 0))
+            .price(20000)
+            .totalSeat(200)
+            .leftSeat(100)
+            .category(Category.뮤지컬)
+            .build();
+
+        validEvent = Event.builder()
             .hostId(1L)
             .title("title")
             .content("content")
