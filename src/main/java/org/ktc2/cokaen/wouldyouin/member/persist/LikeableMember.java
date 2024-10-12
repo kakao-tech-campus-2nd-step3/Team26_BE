9c5471c30d6d69acbb6ab4f2abb7281520bdd7e7
lja3723 <lja3723@gmail.com>
2024-10-13 04:01:54 +0900

    refactor: LikeableMember 명시

--- src/main/java/org/ktc2/cokaen/wouldyouin/member/persist/LikeableMember.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/member/persist/LikeableMember.java
@@ -1,4 +1,10 @@
 package org.ktc2.cokaen.wouldyouin.member.persist;
 
+import java.util.List;
+
 public interface LikeableMember {
+
+    static List<MemberType> getLikeableMemberTypes() {
+        return List.of(MemberType.host, MemberType.curator);
+    }
 }
