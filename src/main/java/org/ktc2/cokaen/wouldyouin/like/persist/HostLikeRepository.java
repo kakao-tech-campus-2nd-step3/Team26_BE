622ab433a6422002beb261012b87af601f0a046b
lja3723 <lja3723@gmail.com>
2024-10-13 03:59:41 +0900

    refactor: Like Entity 계층화

--- src/main/java/org/ktc2/cokaen/wouldyouin/like/persist/HostLikeRepository.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/like/persist/HostLikeRepository.java
@@ -1,11 +1,7 @@
 package org.ktc2.cokaen.wouldyouin.like.persist;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HostLikeRepository extends JpaRepository<HostLike, Long> {
-
-    public List<HostLike> findByMemberId(Long memberId);
+public interface HostLikeRepository extends LikeRepository<HostLike> {
 }
