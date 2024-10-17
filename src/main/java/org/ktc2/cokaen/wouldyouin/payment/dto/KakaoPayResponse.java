fa382d10a455574481eed32b9f6be9240fb75af3
daolove0323 <daolove0323@gmail.com>
2024-10-17 19:25:15 +0900

    refac: 변수 네이밍 변경

--- src/main/java/org/ktc2/cokaen/wouldyouin/payment/dto/KakaoPayResponse.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/payment/dto/KakaoPayResponse.java
@@ -1,5 +1,6 @@
 package org.ktc2.cokaen.wouldyouin.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,11 +9,18 @@ import lombok.Getter;
 @AllArgsConstructor
 public class KakaoPayResponse {
 
+    @JsonProperty("tid")
     private final String tid;
-    private final String next_redirect_app_url;
-    private final String next_redirect_mobile_url;
-    private final String next_redirect_pc_url;
-    private final String android_app_scheme;
-    private final String ios_app_scheme;
-    private final LocalDateTime created_at;
+    @JsonProperty("next_redirect_app_url")
+    private final String nextRedirectAppUrl;
+    @JsonProperty("next_redirect_mobile_url")
+    private final String nextRedirectMobileUrl;
+    @JsonProperty("next_redirect_pc_url")
+    private final String nextRedirectPcUrl;
+    @JsonProperty("android_app_scheme")
+    private final String androidAppScheme;
+    @JsonProperty("ios_app_scheme")
+    private final String iosAppScheme;
+    @JsonProperty("created_at")
+    private final LocalDateTime createdAt;
 }
