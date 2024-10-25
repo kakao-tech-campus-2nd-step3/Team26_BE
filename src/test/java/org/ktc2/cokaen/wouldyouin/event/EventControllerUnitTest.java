3195601be4be85b4ee931ba5573365a6b72a7ad5
조홍식 <90020593+Daolove0323@users.noreply.github.com>
2024-10-25 23:47:15 +0900

    [Weekly/8/Test/Event]이벤트, 예약 도메인 유닛 테스트 수정 (#52)
    * refac: jpaAuditingConfig 클래스 구현
    
    * refac: event unit test 수정
    
    * feat: reservation unit test 수정

--- src/test/java/org/ktc2/cokaen/wouldyouin/event/EventControllerUnitTest.java
+++ src/test/java/org/ktc2/cokaen/wouldyouin/event/EventControllerUnitTest.java
@@ -5,6 +5,8 @@ import static java.lang.Math.abs;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -15,21 +17,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.util.Random;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.ktc2.cokaen.wouldyouin.event.api.EventController;
-import org.ktc2.cokaen.wouldyouin.event.api.dto.EventEditRequest;
-import org.ktc2.cokaen.wouldyouin.event.api.dto.EventCreateRequest;
-import org.ktc2.cokaen.wouldyouin.global.TestData;
+import org.ktc2.cokaen.wouldyouin.auth.application.JwtAuthFilter;
 import org.ktc2.cokaen.wouldyouin.auth.application.JwtService;
+import org.ktc2.cokaen.wouldyouin.event.api.EventController;
+import org.ktc2.cokaen.wouldyouin.event.api.dto.EventCreateRequest;
+import org.ktc2.cokaen.wouldyouin.event.api.dto.EventEditRequest;
 import org.ktc2.cokaen.wouldyouin.event.application.EventService;
+import org.ktc2.cokaen.wouldyouin.global.TestData;
 import org.ktc2.cokaen.wouldyouin.member.application.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@WithMockUser(username = "user", roles = {"USER"})
 @WebMvcTest(EventController.class)
 class EventControllerUnitTest {
 
@@ -42,15 +50,29 @@ class EventControllerUnitTest {
     @MockBean
     private JwtService jwtService;
 
+    @MockBean
+    private JwtAuthFilter jwtAuthFilter;
+
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
 
     private static Long id;
 
     private static ObjectMapper objectMapper;
 
+    @BeforeEach
+    public void setup() throws Exception {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
+
     @BeforeAll
-    public static void setUp() {
+    public static void init() {
         id = abs(new Random().nextLong());
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
@@ -68,7 +90,6 @@ class EventControllerUnitTest {
     void getEventsByHostId() throws Exception {
         mockMvc.perform(get("/api/events/hosts/" + id)).andExpect(status().isOk());
         verify(eventService).getAllByHostId(id);
-
     }
 
     @Test
@@ -81,7 +102,9 @@ class EventControllerUnitTest {
     @Test
     @DisplayName("행사 생성 - 성공")
     void createEvent() throws Exception {
-        mockMvc.perform(post("/api/events").contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(post("/api/events")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(TestData.validEventCreateRequest)))
             .andExpect(status().isCreated());
         verify(eventService).create(any(EventCreateRequest.class));
@@ -90,7 +113,9 @@ class EventControllerUnitTest {
     @Test
     @DisplayName("행사 수정 - 성공")
     void updateEvent() throws Exception {
-        mockMvc.perform(put("/api/events/" + id).contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(put("/api/events/" + id)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(TestData.validEventCreateRequest)))
             .andExpect(status().isOk());
         verify(eventService).update(eq(id), any(EventEditRequest.class));
@@ -99,7 +124,9 @@ class EventControllerUnitTest {
     @Test
     @DisplayName("행사 삭제 - 성공")
     void deleteEvent() throws Exception {
-        mockMvc.perform(delete("/api/events/" + id)).andExpect(status().isNoContent());
+        mockMvc.perform(delete("/api/events/" + id)
+            .with(csrf())
+        ).andExpect(status().isNoContent());
         verify(eventService).delete(id);
     }
 }
