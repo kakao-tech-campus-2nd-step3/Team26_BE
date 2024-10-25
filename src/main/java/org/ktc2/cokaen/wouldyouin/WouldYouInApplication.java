4ae6517070b946a4b26237780ed47f6e91d9ed59
daolove0323 <daolove0323@gmail.com>
2024-10-25 23:16:35 +0900

    refac: jpaAuditingConfig 클래스 구현

--- src/main/java/org/ktc2/cokaen/wouldyouin/WouldYouInApplication.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/WouldYouInApplication.java
@@ -2,14 +2,11 @@ package org.ktc2.cokaen.wouldyouin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class WouldYouInApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WouldYouInApplication.class, args);
     }
-
 }
