97094c05c9810163298b83b14998db41b55377e4
Jangan Lee <lja3723@gmail.com>
2024-10-29 18:56:12 +0900

    [Weekly/9/Hotfix/Issue-49] Issue-49(#49) 해결 (#58)
    * fix: Member 조회시 null이 들어가는 증상 (#49)
    
    * fix: mappedBy 수정

--- src/main/java/org/ktc2/cokaen/wouldyouin/member/persist/BaseMember.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/member/persist/BaseMember.java
@@ -16,10 +16,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 import org.ktc2.cokaen.wouldyouin.Image.persist.MemberImage;
 
-@Slf4j
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -62,25 +60,7 @@ public abstract class BaseMember {
         this.profileImage = profileImage;
     }
 
-    @Override
-    public String toString() {
-        return "BaseMember{" +
-            "Id=" + Id +
-            ", accountType=" + accountType +
-            ", memberType=" + memberType +
-            ", email='" + email + '\'' +
-            ", nickname='" + nickname + '\'' +
-            ", phone='" + phone + '\'' +
-            ", profileImage=" + profileImage +
-            '}';
-    }
-
     public String getProfileImageUrl() {
-        // TODO: 멤버 조회시 profileImage에 null 들어가는 원인 파악 필요
-        log.warn("#### in baseMember.getProfileImageUrl() for baseMember: {}", this);
-        if (profileImage == null || profileImage.isEmpty()) {
-            return null;
-        }
         return profileImage.getFirst().getUrl();
     }
 }
