d501cc9e97d92431eb78e54dd76fc2ce57721452
lja3723 <lja3723@gmail.com>
2024-11-02 22:35:03 +0900

    test: 멤버 도메인 application 패키지 테스트 추가

--- src/main/java/org/ktc2/cokaen/wouldyouin/member/application/dto/request/edit/CuratorEditRequest.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/member/application/dto/request/edit/CuratorEditRequest.java
@@ -1,6 +1,7 @@
 package org.ktc2.cokaen.wouldyouin.member.application.dto.request.edit;
 
 import io.micrometer.common.lang.Nullable;
+import lombok.Builder;
 import lombok.Getter;
 import org.ktc2.cokaen.wouldyouin._common.persist.Area;
 
@@ -9,6 +10,7 @@ public class CuratorEditRequest extends MemberEditRequest {
 
     @Nullable private final String intro;
 
+    @Builder(builderMethodName = "curatorEditRequestBuilder")
     public CuratorEditRequest(@Nullable String nickname, @Nullable String phoneNumber, @Nullable Long profileImageId, @Nullable Area area, @Nullable String intro) {
         super(nickname, phoneNumber, profileImageId, area);
         this.intro = intro;
---
+++ src/test/java/org/ktc2/cokaen/wouldyouin/global/TestUtil.java
@@ -0,0 +1,13 @@
+package org.ktc2.cokaen.wouldyouin.global;
+
+public class TestUtil {
+
+    public static <T> T getLeftOrRight(T left, T right) {
+        return Math.random() > 0.5 ? left : right;
+    }
+
+    public static <T> T getOrNull(T object) {
+        return getLeftOrRight(object, null);
+    }
+
+}
