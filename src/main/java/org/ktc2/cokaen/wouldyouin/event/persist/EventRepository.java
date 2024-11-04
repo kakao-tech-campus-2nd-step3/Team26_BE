7a4265c40d8b0773e911dc62d323d8a9ce512c76
daolove0323 <daolove0323@gmail.com>
2024-11-04 20:02:26 +0900

    fix: JPQL 수정

--- src/main/java/org/ktc2/cokaen/wouldyouin/event/persist/EventRepository.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/event/persist/EventRepository.java
@@ -21,8 +21,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT E FROM Event E JOIN FETCH E.host JOIN FETCH E.images "
         + "WHERE ((:lat1 IS NULL OR :long1 IS NULL OR :lat2 IS NULL OR :long2 IS NULL) "
         + "OR ((E.location.latitude between :lat1 AND :lat2) AND (E.location.longitude between :long1 AND :long2))) "
-        + "AND (:category IS NULL OR E.category = :category) "
-        + "AND (:area IS NULL OR E.area = :area) "
+        + "AND (:category IS NULL OR E.category = '전체' OR E.category = :category) "
+        + "AND (:area IS NULL OR E.area = '전체' OR E.area = :area) "
         + "ORDER BY (:lat - E.location.latitude) * (:lat - E.location.latitude) + "
         + "(:long - E.location.longitude) * (:long - E.location.longitude) ASC")
     Slice<Event> findAllByFilterOrderByDistance(
