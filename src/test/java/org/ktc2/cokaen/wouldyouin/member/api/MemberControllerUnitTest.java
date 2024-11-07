5a5c373d93c757c7ef9fc52465cb53ff6ade9cda
Jangan Lee <lja3723@gmail.com>
2024-11-08 00:41:24 +0900

    [Weekly/10/Test/Member-Controllers] add MemberControllerUnitTest Template (#84)

---
+++ src/test/java/org/ktc2/cokaen/wouldyouin/member/api/MemberControllerUnitTest.java
@@ -0,0 +1,29 @@
+package org.ktc2.cokaen.wouldyouin.member.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(MemberController.class)
+class MemberControllerUnitTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void updateMember() {
+    }
+
+    @Test
+    void findMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+}
