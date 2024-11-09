09e19577482b5c7653b8f2a9ab8c79d8aa2485f1
조홍식 <90020593+Daolove0323@users.noreply.github.com>
2024-11-09 12:02:41 +0900

    [Weekly11/Image/IdSetting] feat: id generation type setting (#99)

--- src/main/java/org/ktc2/cokaen/wouldyouin/Image/persist/Image.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/Image/persist/Image.java
@@ -3,6 +3,7 @@ package org.ktc2.cokaen.wouldyouin.Image.persist;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.validation.constraints.NotNull;
@@ -22,7 +23,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class Image {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     @Column(name = "image_id")
     protected Long id;
