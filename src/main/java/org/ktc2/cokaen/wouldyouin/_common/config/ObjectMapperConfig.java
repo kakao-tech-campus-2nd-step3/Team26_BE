b7144a175c399699b4ec7b318b043872245b2824
조홍식 <90020593+Daolove0323@users.noreply.github.com>
2024-11-09 11:34:31 +0900

    [Weekly/11/Config] feat: objectmapeer bean등록 (#98)

--- src/main/java/org/ktc2/cokaen/wouldyouin/_common/config/ObjectMapperConfig.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/_common/config/ObjectMapperConfig.java
@@ -1,6 +1,7 @@
 package org.ktc2.cokaen.wouldyouin._common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,8 @@ public class ObjectMapperConfig {
 
     @Bean
     ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
     }
 }
