5923b1e0e5c4f882428bf62153a40b7012d44c6e
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-13 00:51:18 +0900

    feat(review): Review 페이지네이션 적용

--- src/main/java/org/ktc2/cokaen/wouldyouin/review/api/ReviewController.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/review/api/ReviewController.java
@@ -1,13 +1,19 @@
 package org.ktc2.cokaen.wouldyouin.review.api;
 
-import java.util.List;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.ktc2.cokaen.wouldyouin._common.api.ApiResponse;
 import org.ktc2.cokaen.wouldyouin._common.api.ApiResponseBody;
-import org.ktc2.cokaen.wouldyouin.review.application.ReviewService;
+import org.ktc2.cokaen.wouldyouin._common.api.ParamDefaults;
+import org.ktc2.cokaen.wouldyouin.auth.Authorize;
+import org.ktc2.cokaen.wouldyouin.auth.MemberIdentifier;
+import org.ktc2.cokaen.wouldyouin.member.persist.MemberType;
 import org.ktc2.cokaen.wouldyouin.review.api.dto.ReviewCreateRequest;
 import org.ktc2.cokaen.wouldyouin.review.api.dto.ReviewEditRequest;
 import org.ktc2.cokaen.wouldyouin.review.api.dto.ReviewResponse;
-import org.springframework.http.HttpStatus;
+import org.ktc2.cokaen.wouldyouin.review.api.dto.ReviewSliceResponse;
+import org.ktc2.cokaen.wouldyouin.review.application.ReviewService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,51 +32,52 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    // ToDo: ApiResponse 정적메서드 활용
-
-    @GetMapping("/{memberId}")
-    public ResponseEntity<ApiResponseBody<List<ReviewResponse>>> getReviewsByMemberId(
-        @PathVariable("memberId") Long memberId) {
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiResponseBody<>(true, reviewService.getAllByMemberId(memberId)));
+    @GetMapping
+    public ResponseEntity<ApiResponseBody<ReviewSliceResponse>> getReviewsByMemberId(
+        @Authorize({MemberType.normal}) MemberIdentifier member,
+        @RequestParam(defaultValue = ParamDefaults.PAGE) Integer page,
+        @RequestParam(defaultValue = ParamDefaults.PAGE_SIZE) Integer size,
+        @RequestParam(defaultValue = ParamDefaults.LAST_ID) Long lastId) {
+        return ApiResponse.ok(
+            reviewService.getAllByMemberId(member.id(), lastId, PageRequest.of(page, size)));
     }
 
-    @GetMapping("/{eventId}")
-    public ResponseEntity<ApiResponseBody<List<ReviewResponse>>> getReviewsByEventId(
-        @PathVariable("eventId") Long eventId) {
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiResponseBody<>(true, reviewService.getAllByEventId(eventId)));
+    @GetMapping("/events/{eventId}")
+    public ResponseEntity<ApiResponseBody<ReviewSliceResponse>> getReviewsByEventId(
+        @PathVariable("eventId") Long eventId,
+        @RequestParam(defaultValue = ParamDefaults.PAGE) Integer page,
+        @RequestParam(defaultValue = ParamDefaults.PAGE_SIZE) Integer size,
+        @RequestParam(defaultValue = ParamDefaults.LAST_ID) Long lastId) {
+        return ApiResponse.ok(
+            reviewService.getAllByEventId(eventId, lastId, PageRequest.of(page, size)));
     }
 
     @GetMapping("/{reviewId}")
     public ResponseEntity<ApiResponseBody<ReviewResponse>> getReviewByReviewId(
         @PathVariable("reviewId") Long reviewId) {
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiResponseBody<>(true, reviewService.getById(reviewId)));
+        return ApiResponse.ok(reviewService.getById(reviewId));
     }
 
-    @PostMapping("/{eventId}")
+    @PostMapping
     public ResponseEntity<ApiResponseBody<ReviewResponse>> createReview(
-        @PathVariable("eventId") Long eventId,
-        @RequestBody ReviewCreateRequest reviewCreateRequest) {
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiResponseBody<>(true, reviewService.create(eventId, reviewCreateRequest)));
+        @Authorize(MemberType.normal) MemberIdentifier member,
+        @Valid @RequestBody ReviewCreateRequest reviewCreateRequest) {
+        return ApiResponse.created(reviewService.create(member.id(), reviewCreateRequest));
     }
 
     @PutMapping("/{reviewId}")
     public ResponseEntity<ApiResponseBody<ReviewResponse>> updateReview(
         @PathVariable("reviewId") Long reviewId,
-        @RequestBody ReviewEditRequest reviewEditRequest) {
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(new ApiResponseBody<>(true, reviewService.update(reviewId, reviewEditRequest)));
+        @Authorize(MemberType.normal) MemberIdentifier member,
+        @Valid @RequestBody ReviewEditRequest reviewEditRequest) {
+        return ApiResponse.ok(reviewService.update(member.id(), reviewId, reviewEditRequest));
     }
 
     @DeleteMapping("/{reviewId}")
     public ResponseEntity<ApiResponseBody<Void>> deleteReview(
-        @PathVariable("reviewId") Long reviewId) {
-        reviewService.delete(reviewId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT)
-            .body(new ApiResponseBody<>(true, null));
+        @PathVariable("reviewId") Long reviewId,
+        @Authorize(MemberType.normal) MemberIdentifier member) {
+        reviewService.delete(member.id(), reviewId);
+        return ApiResponse.noContent();
     }
-
 }
