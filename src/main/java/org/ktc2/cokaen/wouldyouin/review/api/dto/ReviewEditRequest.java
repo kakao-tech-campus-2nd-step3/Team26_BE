069e7c5745809f8216cb5a4d2b423de8b568985f
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-13 00:49:51 +0900

    feat: Review Validatoin 추가

--- src/main/java/org/ktc2/cokaen/wouldyouin/review/api/dto/ReviewEditRequest.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/review/api/dto/ReviewEditRequest.java
@@ -1,5 +1,11 @@
 package org.ktc2.cokaen.wouldyouin.review.api.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,6 +13,12 @@ import lombok.Getter;
 @Builder(toBuilder = true)
 public class ReviewEditRequest {
 
+    @NotBlank(message = "별점은 필수입니다.")
+    @Min(value = 0, message = "별점은 0점 이상입니다. ")
+    @Max(value = 5, message = "별점은 5점 이하입니다. ")
     private Integer score;
+
+    @NotBlank(message = "내용은 필수입니다. ")
+    @Size(min = 5, max = 50, message = "내용은 5자 이상 50자 이하입니다.")
     private String content;
 }
