3d33d64bcd600e1b7ff3a7a43313b38f83daf472
ariimo <memoarim01@gmail.com>
2024-11-14 00:49:37 +0900

    [Weekly/11/Test/event] refactor Event (#125)
    * [Weekly/10/Test/CurationController] curationControllerUnitTest 2 (#90)
    
    * refactor: Event목록 api에서 처리하도록
    
    ---------
    
    Co-authored-by: 조홍식 <90020593+Daolove0323@users.noreply.github.com>

--- src/main/java/org/ktc2/cokaen/wouldyouin/event/api/dto/LocationRequest.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/event/api/dto/LocationRequest.java
@@ -14,12 +14,10 @@ import lombok.Setter;
 @NoArgsConstructor
 public class LocationRequest {
 
-    @NotNull(message = "위도 값은 필수입니다.")
     @Min(value = -90, message = "위도는 -90 이상이어야 합니다.")
     @Max(value = 90, message = "위도는 90 이하여야 합니다.")
     private Double latitude;
 
-    @NotNull(message = "경도 값은 필수입니다.")
     @Min(value = -180, message = "경도는 -180 이상이어야 합니다.")
     @Max(value = 180, message = "경도는 180 이하여야 합니다.")
     private Double longitude;
