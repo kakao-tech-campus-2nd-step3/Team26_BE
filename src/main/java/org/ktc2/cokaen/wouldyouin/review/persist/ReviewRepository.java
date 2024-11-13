58eae0d08e74bae7bf67c05ac5472bf40dabe034
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-14 05:00:17 +0900

    feat: getUnreviewedEventsByMemberId 로직 생성

--- src/main/java/org/ktc2/cokaen/wouldyouin/review/persist/ReviewRepository.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/review/persist/ReviewRepository.java
@@ -1,5 +1,6 @@
 package org.ktc2.cokaen.wouldyouin.review.persist;
 
+import org.ktc2.cokaen.wouldyouin.event.persist.Event;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,4 +19,11 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         + "WHERE R.event.id = :eventId AND R.id > :lastId "
         + "ORDER BY R.id DESC")
     Slice<Review> findByEventIdOrderByReviewIdDesc(Long eventId, Long lastId, Pageable pageable);
+
+    @Query("SELECT DISTINCT e FROM Event e " +
+        "JOIN FETCH e.reservations r " +
+        "WHERE r.member.id = :memberId " +
+        "AND e.id < :lastId " +
+        "AND e.id NOT IN (SELECT rv.event.id FROM Review rv WHERE rv.member.id = :memberId)")
+    Slice<Event> findUnreviewedEventsByMemberId(Long memberId, Long lasId, Pageable pageable);
 }
