7eccf848339b6c5779700ce523fa4bd1e047a7de
ariimo <159447466+ariimo@users.noreply.github.com>
2024-11-14 00:48:23 +0900

    refactor: Event목록 api에서 처리하도록

--- src/main/java/org/ktc2/cokaen/wouldyouin/event/persist/EventRepository.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/event/persist/EventRepository.java
@@ -18,13 +18,9 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Slice<Event> findAllByHostIdOrderByEventIdDesc(Long hostId, Long lastId, Pageable pageable);
 
     @Query("SELECT E FROM Event E JOIN FETCH E.host "
-        + "WHERE E.id > :lastId "
-        + "ORDER BY E.id DESC")
-    Slice<Event> findAllByEventIdDesc(Long lastId, Pageable pageable);
-
-    @Query("SELECT E FROM Event E JOIN FETCH E.host "
-        + "WHERE (E.location.latitude between :startLatitude AND :endLatitude) "
-        + "AND (E.location.longitude between :startLongitude AND :endLongitude) "
+        + "WHERE ((:currentLatitude IS NULL Or :currentLongitude IS NULL) OR "
+        + "((E.location.latitude between :startLatitude AND :endLatitude) "
+        + "AND (E.location.longitude between :startLongitude AND :endLongitude))) "
         + "AND (E.title LIKE %:title%) "
         + "AND (:category = '전체' OR E.category = :category) "
         + "AND (:area = '전체' OR E.area = :area) "
