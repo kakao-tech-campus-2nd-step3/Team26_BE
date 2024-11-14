bdbe9b289072c84990982d4575f6e010518c5c85
Jangan Lee <lja3723@gmail.com>
2024-11-15 07:28:07 +0900

    [Weekly/11/Refactor/TokenResponse] token response에 사용자 아이디, 타입 반환하도록 반영 (#139)

--- src/main/java/org/ktc2/cokaen/wouldyouin/auth/api/dto/SocialTokenResponse.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/auth/api/dto/SocialTokenResponse.java
@@ -1,8 +1,9 @@
 package org.ktc2.cokaen.wouldyouin.auth.api.dto;
 
 import lombok.Builder;
+import org.ktc2.cokaen.wouldyouin.member.persist.MemberType;
 
 @Builder
-public record SocialTokenResponse(Boolean isWelcomeMember, String token) {
+public record SocialTokenResponse(Boolean isWelcomeMember, String token, Long memberId, MemberType memberType) {
 
 }
