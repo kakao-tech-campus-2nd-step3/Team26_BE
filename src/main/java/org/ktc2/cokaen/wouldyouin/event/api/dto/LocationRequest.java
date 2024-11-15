cfa70465eaee9749950d4279a4b02aa3ed2feacc
조홍식 <90020593+Daolove0323@users.noreply.github.com>
2024-11-15 10:30:30 +0900

    [Weekly/11]  not null 속성 수정 (#143)

--- src/main/java/org/ktc2/cokaen/wouldyouin/event/api/dto/LocationRequest.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/event/api/dto/LocationRequest.java
@@ -18,12 +18,10 @@ import lombok.ToString;
 @ToString
 public class LocationRequest {
 
-    @NotNull(message = "위도는 필수값입니다.")
     @Min(value = -90, message = "위도는 -90 이상이어야 합니다.")
     @Max(value = 90, message = "위도는 90 이하여야 합니다.")
     private Double latitude;
 
-    @NotNull(message = "경도는 필수값입니다.")
     @Min(value = -180, message = "경도는 -180 이상이어야 합니다.")
     @Max(value = 180, message = "경도는 180 이하여야 합니다.")
     private Double longitude;
