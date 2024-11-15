ecf26ffdf75ee9190ca6aee944c641a77e1d86ae
ariimo <memoarim01@gmail.com>
2024-11-15 16:38:52 +0900

    [Weekly/11/Refactor/ErrorCode] ErrorCode 수정 (#147)

--- src/main/java/org/ktc2/cokaen/wouldyouin/_common/exception/ErrorCode.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/_common/exception/ErrorCode.java
@@ -4,56 +4,36 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-// TODO: code 수정 필요
 public enum ErrorCode {
 
-    FAIL_SOCIAL_DATA_GET(HttpStatus.UNAUTHORIZED.value(), "-20400"),
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "-40904"),
+    FAIL_TO_DELETE_IMAGE(HttpStatus.CONFLICT.value(), "-40903"),
+    FAIL_TO_UPLOAD_IMAGE(HttpStatus.CONFLICT.value(), "-40902"),
+    FAIL_TO_PAY(HttpStatus.CONFLICT.value(), "-40901"),
+    FAIL_TO_READ_IMAGE(HttpStatus.CONFLICT.value(), "-40900"),
 
-    FAIL_ACCESS_TOKEN_GET(HttpStatus.UNAUTHORIZED.value(), "-20400"),
+    RESERVATION_NOT_FOUND_FOR_REVIEW(HttpStatus.NOT_FOUND.value(), "40401"),
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "-40400"),
 
-    AUTHENTICATION_EMPTY(HttpStatus.UNAUTHORIZED.value(), "-20400"),
+    SECURITY_FILTER_AUTHORIZATION_REQUIRED(HttpStatus.FORBIDDEN.value(), "-40300"),
 
-    JWT_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED.value(), "-20400"),
+    FAIL_SOCIAL_DATA_GET(HttpStatus.UNAUTHORIZED.value(), "-40106"),
+    FAIL_ACCESS_TOKEN_GET(HttpStatus.UNAUTHORIZED.value(), "-40105"),
+    AUTHENTICATION_EMPTY(HttpStatus.UNAUTHORIZED.value(), "-40104"),
+    JWT_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED.value(), "-40103"),
+    ADDITIONAL_INFO_ILLEGAL_ACCESS(HttpStatus.UNAUTHORIZED.value(), "-40102"),
+    LOGIN_FAILED(HttpStatus.UNAUTHORIZED.value(), "-40101"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), "-40100"),
 
-    INVALID_AUTHORIZATION_HEADER(HttpStatus.BAD_REQUEST.value(), "-20400"),
-
-    SECURITY_FILTER_AUTHORIZATION_REQUIRED(HttpStatus.FORBIDDEN.value(), "-20400"),
-
-    SECURITY_FILTER_FORBIDDEN_ACCESS(HttpStatus.BAD_REQUEST.value(), "-20400"),
-
-    NOT_LIKEABLE_MEMBER_TYPE(HttpStatus.BAD_REQUEST.value(), "-20400"),
-
-    ADDITIONAL_INFO_ILLEGAL_ACCESS(HttpStatus.UNAUTHORIZED.value(), "-20400"),
-
-    LOGIN_FAILED(HttpStatus.UNAUTHORIZED.value(), "-20400"),
-
-    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "-10400"),
-
-    CURRENT_LOCATION_EMPTY(HttpStatus.BAD_REQUEST.value(), "-20400"),
-
-    INVALID_IMAGE_DOMAIN(HttpStatus.BAD_REQUEST.value(), "-20400"),
-
-    FAIL_TO_DELETE_IMAGE(HttpStatus.CONFLICT.value(), "-20400"),
-
-    FAIL_TO_UPLOAD_IMAGE(HttpStatus.CONFLICT.value(), "-20400"),
-
-    FAIL_TO_PAY(HttpStatus.CONFLICT.value(), "-20400"),
-
-    FAIL_TO_READ_IMAGE(HttpStatus.CONFLICT.value(), "-20400"),
-
-    RESERVATION_NOT_FOUND_FOR_REVIEW(HttpStatus.NOT_FOUND.value(), "14044"),
-
-    NO_LEFT_SEAT(HttpStatus.BAD_REQUEST.value(), "-10400"),
-
-    URL_PARSING_FAILED(HttpStatus.BAD_REQUEST.value(), "-20400"),
-
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST.value(), "-20400"),
-
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), "-20400"),
-
-    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "-10404"),
-
-    UNEXPECTED(HttpStatus.BAD_REQUEST.value(), "-1");
+    INVALID_AUTHORIZATION_HEADER(HttpStatus.BAD_REQUEST.value(), "-40008"),
+    SECURITY_FILTER_FORBIDDEN_ACCESS(HttpStatus.BAD_REQUEST.value(), "-40007"),
+    NOT_LIKEABLE_MEMBER_TYPE(HttpStatus.BAD_REQUEST.value(), "-40006"),
+    CURRENT_LOCATION_EMPTY(HttpStatus.BAD_REQUEST.value(), "-40005"),
+    INVALID_IMAGE_DOMAIN(HttpStatus.BAD_REQUEST.value(), "-40004"),
+    NO_LEFT_SEAT(HttpStatus.BAD_REQUEST.value(), "-40003"),
+    URL_PARSING_FAILED(HttpStatus.BAD_REQUEST.value(), "-40002"),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST.value(), "-40001"),
+    UNEXPECTED(HttpStatus.BAD_REQUEST.value(), "-40000");
 
     private final Integer status;
     private final String code;
