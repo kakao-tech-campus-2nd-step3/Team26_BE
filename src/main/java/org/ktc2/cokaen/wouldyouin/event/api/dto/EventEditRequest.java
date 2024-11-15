57a8df7ea60eddbe8559aba2e0a526203c1e781f
ariimo <memoarim01@gmail.com>
2024-11-15 09:34:59 +0900

    [Weekly/11/Refactor/All] Transactional 설정, 접근제어자 설정 (#142)
    * refactor: Review Transactional 설정
    
    * refactor: RequestDto 접근제어자 private으로 변경

--- src/main/java/org/ktc2/cokaen/wouldyouin/event/api/dto/EventEditRequest.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/event/api/dto/EventEditRequest.java
@@ -62,7 +62,7 @@ public class EventEditRequest {
     private List<Long> imageIds;
 
     @AssertTrue(message = "종료 시간은 시작 시간 이후여야 합니다.")
-    public boolean isEndTimeAfterStartTime() {
+    private boolean isEndTimeAfterStartTime() {
         if (startTime == null || endTime == null) {
             return true;
         }
@@ -70,7 +70,7 @@ public class EventEditRequest {
     }
 
     @AssertTrue(message = "이미지는 최대 5개까지 등록할 수 있습니다.")
-    public boolean isImageSizeValid() {
+    private boolean isImageSizeValid() {
         return imageIds.size() <= 5;
     }
 }
